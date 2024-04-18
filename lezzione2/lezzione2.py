737c99fdf314b6800495a13e4fc03e04554c1164
NotFrenk <franciscoleonfelici@gmail.com>
2024-04-18 13:36:56 +0200

    risolto esercizio 2.3

---
+++ lezzione2/lezzione2.py
@@ -0,0 +1,12 @@
+#Francisco felici
+#18/04/24
+#es 2.3
+
+nome:str="Andrea"
+
+messaggio:str= f"ciao {nome} andiamo a studiare python insime?"
+
+print(messaggio)
+
+#----------------------------------------------------------------------------
+#es 2.4
