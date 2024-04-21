3d98b6499395576aaa41c3d49fddb2c3dbfd4f6d
NotFrenk <franciscoleonfelici@gmail.com>
2024-04-21 22:08:22 +0200

    lezione2 escape characters

---
+++ lezzione2/Escape_Characters.py
@@ -0,0 +1,5 @@
+print("Hello\nWorld!") #manda a capo
+print("Name:\tAlice")  #distanzia la parola successiva
+print("C:\\Users\\Alice\\Documents") #aggiunge un "\"
+print('It\'s a sunny day.') #aggiunge l'apostrofo
+print("She said, \"Hello!\".") #aggiunge le virgolette
