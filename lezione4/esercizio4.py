dece4fd30d142e6a7e62a08894f022ea9779b248
NotFrenk <franciscoleonfelici@gmail.com>
2024-04-22 18:42:35 +0200

    esercizio4

---
+++ lezione4/esercizio4.py
@@ -0,0 +1,14 @@
+"""data una lista nums di n elementi, restituire l'elemento che 
+compare piu di n/2 volte
+
+esempio 1
+nums= [3,2,3] -> restituisce 3
+"""
+
+def majority_element(nums:list[int]) -> int:
+    for i in nums:
+        if nums.count(i) > len(nums) /2:
+            print(i)
+            return i
+    return print("non ci siamo")
+majority_element([3,7,6,2,3])
