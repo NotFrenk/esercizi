8ba66827c7a761c8e0e71b9877c0c0d530357055
NotFrenk <franciscoleonfelici@gmail.com>
2024-04-23 18:14:21 +0200

    es 2-4

--- lezione2/lezzione2.py
+++ lezione2/lezzione2.py
@@ -1,16 +1,17 @@
 #Francisco felici
 #18/04/24
 #es 2.3
-
+"""
 nome:str="Andrea"
 
 messaggio:str= f"ciao {nome} andiamo a studiare python insime?"
 
 print(messaggio)
-
+"""
 #----------------------------------------------------------------------------
 #es 2.4
-
+#Use a variable to represent a person’s name,
+#and then print that person’s name in lowercase, uppercase, and title case.
 
 nome:str="Andrea"
 
@@ -18,5 +19,7 @@ nome_lower:str= nome.lower()
 
 nome_upper:str= nome.upper()
 
-print(f"{nome}, {nome_lower}, {nome_upper}")
+nome_title:str=nome.title()
+
+print(f"{nome}, {nome_lower}, {nome_upper}, {nome_title}")
 
