e316894ae52794b1a6db8b8ea54072240e251cf0
NotFrenk <franciscoleonfelici@gmail.com>
2024-04-23 18:15:29 +0200

    esercizio dizionario

--- lezione2/esercizi_dizionari.py
+++ lezione2/esercizi_dizionari.py
@@ -1,6 +1,20 @@
-#We have a menu at a restaurant.
-#The available dishes are: pasta, pizza, salad, wine, water
-#Their prices are: 10.50, 9.00, 6.50, 4.00, 2.30
-#1. Save this information into a dictionary called menu.
-#2. From menu, access the prices of pasta and wine. Save them in separate variables.
-#3. Print the prices of pasta and wine.
+
+"""
+We have a menu at a restaurant.
+The available dishes are: pasta, pizza, salad, wine, water
+Their prices are: 10.50, 9.00, 6.50, 4.00, 2.30
+1. Save this information into a dictionary called menu.
+2. From menu, access the prices of pasta and wine. Save them in separate variables.
+3. Print the prices of pasta and wine.ù
+"""
+menù:dict ={
+    "pasta":10.50,
+    "pizza":9.00,
+    "insalata":6.50,
+    "vino":4.00,
+    "acqua":2.30
+    }
+prezzo_pasta=menù["pasta"]
+prezzo_vino=menù["vino"]
+print("prezzo pasta:", prezzo_pasta)
+print("prezzo del vino:",prezzo_vino)
