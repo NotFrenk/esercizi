4ac53fafc8b7842ae24c0cfed64e73067b8431b7
NotFrenk <franciscoleonfelici@gmail.com>
2024-04-23 18:18:39 +0200

    es 2.5 iniziato

--- lezione2/lezzione2.py
+++ lezione2/lezzione2.py
@@ -13,6 +13,8 @@ print(messaggio)
 #Use a variable to represent a person’s name,
 #and then print that person’s name in lowercase, uppercase, and title case.
 
+"""
+
 nome:str="Andrea"
 
 nome_lower:str= nome.lower()
@@ -23,3 +25,12 @@ nome_title:str=nome.title()
 
 print(f"{nome}, {nome_lower}, {nome_upper}, {nome_title}")
 
+"""
+#-----------------------------------------------------------------------
+#es: 2-5.
+#Famous Quote: Find a quote from a famous person you admire.
+#Print the quote and the name of its author. Your output should look something like the following,
+#including the quotation marks: Albert Einstein once said,
+#“A person who never made a mistake never tried anything new.”
+
+""""""
