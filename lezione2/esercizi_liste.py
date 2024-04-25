af0ee42ebf082974e547cdab9c87ff6fce4720d7
NotFrenk <franciscoleonfelici@gmail.com>
2024-04-25 23:42:41 +0200

    modifica nomi cartella

---
+++ lezione2/esercizi_liste.py
@@ -0,0 +1,9 @@
+"""
+1. Define a list called alphabet. It should contain the first 5 letters of the alphabet.
+2. Define the variables first_letter and last_letter. Fill them accordingly.
+3. Define the variable first_three. Use slicing to fill this variable.
+4. Define the variable last_three. Use slicing to fill this variable.
+5. Add the next 3 letters of the alphabet to our alphabet.
+6. Now, update your variable last_three.
+7. Delete the last letter from our alphabet.
+"""
