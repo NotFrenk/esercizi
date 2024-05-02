a9715193d573b8d66e5e1d52a30871c26ea493cc
NotFrenk <franciscoleonfelici@gmail.com>
2024-05-02 10:32:36 +0200

    esercizi ripasso

---
+++ lezione5/lezione5.py
@@ -0,0 +1,14 @@
+#Scrivi una funzione che accetti tre parametri: 
+#username, password e status di attivazione dell'account (attivo/non attivo). 
+#L'accesso è consentito solo se il nome utente è "admin", 
+#la password corrisponde a "12345" e l'account è attivo. 
+#La funzione ritorna "Accesso consentito" oppure "Accesso negato".
+"""
+
+"""
+def funzione(username,password):
+    if username == "admin" and password == 12345:
+        print("accesso consentito")
+    else:
+        print("accesso negato")
+funzione("admin", 12345)
