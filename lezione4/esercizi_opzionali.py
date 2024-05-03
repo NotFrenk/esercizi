404f7d34d978463491d465537ffd4c7c438bf790
NotFrenk <franciscoleonfelici@gmail.com>
2024-05-03 09:07:58 +0200

    esercizi opzionali lezione 4

---
+++ lezione4/esercizi_opzionali.py
@@ -0,0 +1,8 @@
+#1. School Grading System:
+#Create a function that takes a student's name and their scores in different subjects as input.
+#The function calculates the average score and prints the student's name, average, 
+#and a message indicating whether the student passed the exam (average >= 60) or failed.
+#Create a for loop to iterate over a list of students and scores, calling the function for each student.
+"""
+
+"""
