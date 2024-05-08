1910a8160a2fc0397084eb9baefe7703d8fc131c
NotFrenk <franciscoleonfelici@gmail.com>
2024-05-08 16:53:35 +0200

    inizio esercizi lezione 6

--- lezione6/lezione6.py
+++ lezione6/lezione6.py
@@ -1,18 +1,46 @@
-class Person:
-    def __init__(self,name:str, surname:str, age:int):
-        self.name = name
-        self.surname = surname
-        self.age = age
+class Restaurant:
+    def __init__(self, name:str, cuisin_type:str):
+        self.name: str = name
+        self.cuisin_type: str = cuisin_type
+    
+    def describe_restourant(self):
+        print(f"Restaurant(Name={self.name},"\
+              + f"cusine={self.cuisin_type})")
+        
+    def open_restaurant(self):
+        print(f"il ristorante {self.name} è aperto")
 
-"""
-name : str = input()
-surname : str = input()
-age: int = input()
-"""
-
-person = Person("lorenzo", "maggi", 24)
-person = Person("oussama", "Hiliwa", 21)
-print(f"nome = {lorenzo.name}, cognome = {lorenzo.surname}, età = {lorenzo.age}")
+        #da finire
 
 
 
+#----------------------------------------------------------------------------------------------------
+
+class Utente:
+
+    def __init__ (self,
+                  first_name:str,
+                  second_name:str,
+                  age:str,
+                  cf:str,
+                  email:str):
+        
+        self.first_name : str = first_name
+        self.second_name : str = second_name
+        self.age : int = age
+        self.cf : str = cf
+        self.email : str = email
+
+    def __str__ (self):
+        return f"user(name={self.first_name},"\
+            +f"surname={self.second_name})"
+
+user1 = Utente(
+    first_name="lorenzo"
+    second_name="maggi"
+    age=
+)
+
+print(user1)
+
+#da finire
