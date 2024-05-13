4719a3eaebfb4468d163af5501cdf92abdee6520
NotFrenk <franciscoleonfelici@gmail.com>
2024-05-14 00:39:10 +0200

    continuo zoo

--- lezione6/zoo.py
+++ lezione6/zoo.py
@@ -1,3 +1,17 @@
+class Zoo:
+    def __init__(self, fences=None, zoo_keepers=None):
+        self.fences = []
+        self.zoo_keepers = []
+    
+    def add_fence(self, fence):
+        self.fences.append(fence)
+    
+    def add_zoo_keeper(self, zoo_keeper):
+        self.zoo_keepers.append(zoo_keeper)
+
+
+
+
 class Animal:
     def __init__ (self, name:str, species:str, age:int, height:float, width:float, preferred_habitat:str, health:float):
 
@@ -14,30 +28,45 @@ class Animal:
 
 class Fence:
     def __init__(self, area:float, temperature:float, habitat:str):
-        self.area:float = area
-        self.temperature:float = temperature
-        self.habitat:str = habitat
+        if animals is None:
+            animals = []
+        self.area = area
+        self.temperature = temperature
+        self.habitat = habitat
+        self.animals = animals
 
+    def add_animal(self, animal):
+        self.animals.append(animal)
 
-class ZooKeeper:
-    def __init__(self, name:str, surname:str, id:str,):
+class ZooKeeper(Fence):
+    def __init__(self, name:str, surname:str, id:str, area: float, temperature: float, habitat: str):
+        super().__init__(area, temperature, habitat)
         self.name:str=name
         self.surname:str = surname
         self.id:str = id
 
     def add_animal(animal: Animal, fence: Fence):
-        if animal.height and animal.width <= fence.area:
+        if fence.area >= animal.height * animal.width:
+             if fence.habitat == animal.preferred_habitat:
+                fence.add_animal(animal)
+        
+    def remove_animal(self, animal, fence):
+        if animal in fence.animals:
+            fence.remove_animal(animal)
+            fence.restore_area(animal)
             
-            print("entgra")
+
+        
+            
+
+            
+            
 
 
 
 
 
-class Zoo:
-    def __init__(self, fences:list[Fence], zoo_keepers:list[ZooKeeper]):
-        self.fences:list[Fence] = fences
-        self.zoo_keepers:list[ZooKeeper] = zoo_keepers
+
 
 
 
