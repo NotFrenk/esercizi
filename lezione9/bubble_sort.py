06efa8b424925a8e9323c5267d1844ae8cfb6108
NotFrenk <franciscoleonfelici@gmail.com>
2024-05-15 10:07:40 +0200

    bubble sort migliorato

--- lezione9/bubble_sort.py
+++ lezione9/bubble_sort.py
@@ -1,6 +1,7 @@
 # ASYMPTOTIC NOTATION & BUBBLE SORT
 
-def bubble_sorte(x:list[float])
+"""ORIGINALE"""
+def bubble_sorte(x:list[float]):
     for i in range(len(x)):
         for j in  range(len(x) - 1):
             if x[j] > x[j + 1]:
@@ -9,4 +10,12 @@ def bubble_sorte(x:list[float])
                 x[j] = x[j + 1]
                 
 
-
+"""MIGLIORAMENTO"""
+# linea 17
+def bubble_sorte_migliorato(x:list[float]):
+    for i in range(len(x)):
+        for j in  range(len(x) - i - 1):
+            if x[j] > x[j + 1]:
+                temp: float= x[j + 1]
+                x[j + 1] = x[j]
+                x[j] = x[j + 1]
