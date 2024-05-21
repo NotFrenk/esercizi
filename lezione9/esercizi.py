d22fbfd09f70d64dcb950a84f4c4e2c6ada52169
NotFrenk <franciscoleonfelici@gmail.com>
2024-05-21 15:43:45 +0200

    soluzione: sudoku , lista concatenata

--- lezione9/esercizi.py
+++ lezione9/esercizi.py
@@ -87,7 +87,7 @@ print(symmetric([1,2,2,3,4,4,3]))
             #get_balance(account_id): restituisce il saldo del conto con l'ID specificato.
 
 
-
+"""
 class Account:
     def __init__(self, account_id: str ):
         self.account_id = account_id
@@ -144,7 +144,7 @@ bank = Bank()
 account2 = bank.create_account("456")
 bank.deposit("456",200)
 print(bank.get_balance("456"))
-
+"""
 
 
 
@@ -152,21 +152,38 @@ print(bank.get_balance("456"))
 
 """
 class ListNode:
+    
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
         
-def reverse_list(head: ListNode) -> list[int]:
-    prev=None
-    next=head
-
+def reverse(head: ListNode) -> list[int]:
+    reversed_list: list[int] = []
+    queue: list[ListNode] = [head]
     
+    while queue:
+        curr_node = queue.pop()
+        if curr_node:
+            reversed_list.append(curr_node.val)
+            queue.append(curr_node.next)
+    return reversed_list[::-1]
 
+def reverse_recursive(head: ListNode) -> list[int]:
+    reversed_list: list[int] = []
+    _reverse(head, reversed_list)
+    return reversed_list[::-1]
 
+def _reverse(curr_node: ListNode, reversed_list: list[int]):
+    if curr_node:
+        reversed_list.append(curr_node.val)
+        _reverse(curr_node.next, reversed_list)
+        
 
-
-head = ListNode(val=1, next=ListNode(val=2))
-print(reverse_list(head))
+head = ListNode(val=0, 
+                next=ListNode(val=1, 
+                              next=ListNode(val=5, 
+                                            next=ListNode(val=6))))
+print(reverse(head))
 """
 
 
@@ -287,3 +304,51 @@ class Library:
             return borrowed_books
         else:
             raise ValueError("Member ID not found.")"""
+
+
+
+
+# Determina se una tavola Sudoku 9 x 9 è valida. Solo le celle compilate devono essere convalidate secondo le seguenti regole:
+
+#     Ogni riga deve contenere le cifre 1-9 senza ripetizioni.
+#     Ciascuna colonna deve contenere le cifre da 1 a 9 senza ripetizioni.
+#     Ciascuno dei nove sottoriquadri 3 x 3 della griglia deve contenere le cifre 1-9 senza ripetizione.
+
+# Nota:
+
+#     Una tavola Sudoku (parzialmente riempita) potrebbe essere valida ma non è necessariamente risolvibile.
+#     Solo le celle riempite devono essere convalidate secondo le regole menzionate.
+
+"""
+def sudoku(tavola: list[list[int]]) -> bool:
+    rows: dict[int, list[int]] = {i: [] for i in range(9)}
+    # rows = {0: [], 1: [], ... , 8: []}
+    cols: dict[int, list[int]] = {i: [] for i in range(9)}
+    squares: dict[int, list[int]] = {f'{i}-{j}': [] for i in range(3) for j in range(3)}
+    # squares = dict[int, list[int]] = {i: [] for i in range(9)
+    for i in range(9):
+       for j in range(9):
+           curr_elem: str = tavola[i][j]
+           if curr_elem != '.':
+                square_i, square_j = i // 3, j // 3
+                # square_index = 3 * square_i + square_j
+                # if curr_elem in rows[i] or curr_elem in cols[j] or curr_elem in squares[square_index]
+                if curr_elem in rows[i] or curr_elem in cols[j] or curr_elem in squares[f'{square_i}-{square_j}']:
+                    return False
+                
+                rows[i].append(curr_elem)
+                cols[j].append(curr_elem)
+                squares[f'{square_i}-{square_j}'].append(curr_elem)
+                # squares[square_index].append(curr_elem)
+    return True
+                
+# square_i = 0, square_j = 0 -> 0
+# square_i = 0, square_j = 1 -> 1
+# square_i = 0, square_j = 2 -> 2
+# square_i = 1, square_j = 0 -> 3
+# square_i = 1, square_j = 1 -> 4
+# square_i = 1, square_j = 2 -> 5
+# square_i = 2, square_j = 0 -> 6
+# square_i = 2, square_j = 1 -> 7
+# square_i = 2, square_j = 2 -> 8 ---> 3 * square_i + square_j
+"""
