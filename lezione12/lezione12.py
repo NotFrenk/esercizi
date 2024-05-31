16a0cdced582820d03813d044dd8d9a8517c7a1c
NotFrenk <franciscoleonfelici@gmail.com>
2024-05-31 10:55:46 +0200

    Spiegazione assert

---
+++ lezione12/lezione12.py
@@ -0,0 +1,15 @@
+
+# i: int =0
+# test_value:int = 5
+
+# #verifica se una condizione è vera
+# assert i == test_value, f'error\n the variable i must be {test_value} insted is {i}'
+
+
+def sum(a:int, b:int):
+    return 0
+
+result = sum(a=5, b=2)
+test_value: int = 7
+assert result == test_value, f'error\n the variable i must be {test_value} insted is {result}'
+
