97fb8f86a4d6dbe341efa7e8fa99d3e46097fed4
NotFrenk <franciscoleonfelici@gmail.com>
2024-06-14 13:03:05 +0200

    classe paziente finita

--- lezione17/gestione_ospedaliera/paziente.py
+++ lezione17/gestione_ospedaliera/paziente.py
@@ -19,4 +19,38 @@
 #         "Paziente: {nome} {cognome}
 #          ID: {codice identificativo}"
 
-class Paziente:
+from persona import Persona
+
+class Paziente(Persona):
+    def __init__(self, first_name, last_name, idCode):
+        super().__init__(first_name, last_name)
+        
+        if isinstance(idCode, str):
+            self.__idCode = idCode
+        else:
+            self.__idCode = None
+            print("Il codice identificativo inserito non è una stringa!")
+
+    def setIdCode(self, idCode):
+        if isinstance(idCode, str):
+            self.__idCode = idCode
+        else:
+            print("Il codice identificativo inserito non è una stringa!")
+
+    def getidCode(self):
+        return self.__idCode
+    
+    def patientInfo(self):
+        if self.__idCode is not None:
+            print(f"Paziente: {self.getName()} {self.getLastname()} \n ID: {self.__idCode}")
+        else:
+            print("Informazioni incomplete sul paziente!")
+
+
+# paziente = Paziente("Marco", "Bianchi", "123456")
+# paziente.greet()
+# paziente.patientInfo()
+# paziente.setIdCode(789)  # Errore: Il codice identificativo inserito non è una stringa!
+# paziente.setIdCode("789012")
+# print(paziente.getidCode())  # 789012
+# paziente.patientInfo()
