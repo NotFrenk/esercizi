3e925e43b33388164649671c0391dc76e4cce4f1
NotFrenk <franciscoleonfelici@gmail.com>
2024-06-27 12:36:27 +0200

    lezione 21

--- RENDERING GRAFICO.py
+++ RENDERING GRAFICO.py
@@ -5,17 +5,60 @@
 # Definire la classe astratta Forma che sarà utilizzata per definire l'attributo corrispondente al nome della forma e le funzionalità base di ogni forma, 
 # come i metodi astratti getArea() per calcolare l'area e render() per disegnare su schermo la forma.
 
+from abc import (ABC) 
+
+class Forma(ABC):
+    def __init__(self, nome):
+        self.nome=nome
+
+    def getArea(self):
+        pass
+
+    def render(self):
+        pass
+
 # Definire la classe Quadrato che estende la classe Forma e aggiunge specifiche circa la lunghezza di un suo lato.
 # Il costruttore della classe deve ricevere come argomento solo il lato del quadrato, ed impostare il nome della forma su "Quadrato".
 # Il metodo getArea() deve calcolare l'area del quadrato.
 # Il metodo render() deve stamapre su schermo un quadrato avente lato pari al valore passato nel costruttore. 
 # Il quadrato da stampare deve essere un quadrato vuoto (" "), avente degli asterischi ("*") lungo il suo perimetro. (Vedi Esempio di output)
+ 
+class Quadrato(Forma):
+    def __init__(self, lunghezza_lato):
+        super().__init__("Quadrato")
+        self.lunghezza_lato=lunghezza_lato
+
+    def getArea(self):
+        return self.lunghezza_lato * self.lunghezza_lato
+    
+    def render(self):
+        print(f'Ecco un {self.nome} di lato {self.lunghezza_lato}')
+        for i in range(self.lunghezza_lato):
+            for j in range(self.lunghezza_lato):
+                if i == 0 or i == self.lunghezza_lato -1 or j == 0 or j == self.lunghezza_lato -1:
+                    print('*', end=' ')
+                else:
+                    print(' ', end=' ')
+            print()
+        print (f'L\'area di questo {self.nome} vale: {self.getArea()}')
 
 # Definire la classe Rettangolo che estende la classe Forma e aggiunge specifiche circa la lunghezza della sua base e della sua altezza.
 # Il costruttore della classe deve ricevere come argomento solo la base e l'altezza del rettangolo, ed impostare il nome della forma su "Rettangolo".
 # Il metodo getArea() deve calcolare l'area del rettangolo.
 # Il metodo render() deve stampare su schermo un rettangolo avente base ed altezza pari ai valori passati nel costruttore. 
 # Il rettangolo da stampare deve essere un rettangolo vuoto (" "), avente degli asterischi ("*") lungo il suo perimetro. (Vedi Esempio di output)
+
+class Rettangolo(Forma):
+    def __init__(self, base, altezza):
+        super().__init__('Rettangolo')
+        self.base=base
+        self.altezza=altezza 
+    
+    def getArea(self):
+        return self.base * self.altezza
+    
+    def render(self):
+        print()
 
 # Definire la classe Triangolo che estende la classe Forma e aggiunge specifiche circa la dimensione di un lato del triangolo 
 # (per semplicità, si suppone che il triangolo in questione sia un triangolo rettangolo).
@@ -31,15 +74,15 @@
 # Ecco un Quadrato di lato 4!
 
 # * * * *
-# *      *
-# *      *
+# *     *
+# *     *
 # * * * *
 # L'area di questo quadrato vale: 16
 
 # Ecco un Rettangolo avente base 8 ed altezza 4!
 # * * * * * * * *
-# *                *
-# *                *
+# *             *
+# *             *
 # * * * * * * * *
 # L'area di questo rettangolo vale: 32
 
@@ -49,3 +92,13 @@
 # *   *  
 # * * * *
 # L'area di questo triangolo vale: 8.0
+
+
+
+
+
+
+
+# Esempi di utilizzo:
+quadrato = Quadrato(4)
+quadrato.render()
---
+++ lezione21/lezione_21.py
@@ -0,0 +1,36 @@
+# def generatore():
+#     yield "A"
+#     yield 'B'
+#     yield 'C'
+
+# prova_generatore = generatore()
+
+# print(next(prova_generatore))
+# print(next(prova_generatore))
+# print(next(prova_generatore))
+# print(next(prova_generatore))
+
+
+
+
+import time 
+from contextlib import contextmanager
+
+@contextmanager
+def contex_manager_decorator():
+
+    start_time = time.time()
+
+    yield
+
+    end_tieme = time.time()
+    elapsed_time = end_tieme - start_time
+
+    print(f'{elapsed_time=}')
+
+@contex_manager_decorator
+def area_cerchio(raggio: float):
+
+    return raggio * raggio * 3.14
+
+area_cerchio(1)
