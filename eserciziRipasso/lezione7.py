8563f75e9d6a6c8ae1b9ddcf5d2d07ff31f41372
NotFrenk <franciscoleonfelici@gmail.com>
2024-07-21 17:25:06 +0200

    3° esercizio di ripasso

--- eserciziRipasso/lezione7.py
+++ eserciziRipasso/lezione7.py
@@ -22,7 +22,7 @@ print(prendi_valore(dizionario1, 5))
 # 2. 
 # Scrivi una funzione che inverte le chiavi e i valori in un dizionario. 
 # Se ci sono valori duplicati, scarta i duplicati.
-
+"""
 def inverti_oggetti(dizionario:dict):
     
     dizionario_invertito = {}
@@ -45,3 +45,24 @@ dizionario1 = {
 
 dizionario_invertito = inverti_oggetti(dizionario1)
 print(dizionario_invertito)
+"""
+
+# 3. 
+# Scrivi una funzione che riceve una lista di numeri, filtra i numeri pari, 
+# e restituisce una nuova lista con i numeri pari moltiplicati per un fattore dato.
+"""
+def filtra_lista(lista:list, fattore:int):
+
+    new_list:list = []
+
+    for numero in lista:
+        if numero % 2 == 0:
+            numero *= fattore
+            new_list.append(numero)
+    print(f'{new_list}')
+
+
+numeri = [1,2,3,4,5,6,7,8,9,10]
+fattore = 3
+numeri_filtrati = filtra_lista(numeri, fattore)
+"""
