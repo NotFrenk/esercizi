0e7f96172f6d8ae5fa16198a8d5657af7876090c
NotFrenk <franciscoleonfelici@gmail.com>
2024-07-23 09:17:32 +0200

    esercizi per esame

---
+++ eserciziRipasso/esercizi esame/tre_parametri.py
@@ -0,0 +1,13 @@
+#Scrivi una funzione che accetti tre parametri: username, password e status di attivazione dell'account (attivo/non attivo). 
+# L'accesso è consentito solo se il nome utente è "admin", la password corrisponde a "12345" e l'account è attivo. 
+# La funzione ritorna "Accesso consentito" oppure "Accesso negato".
+
+def check_access(username: str, password:int , is_active: bool) -> str:
+    if username == 'admin' and password == '12345' and is_active:
+        return 'Accesso consentito'
+    else:
+        return 'Accesso negato'
+
+
+print(check_access("admin", "12345", True))
+print(check_access("admin", "54321", True))
