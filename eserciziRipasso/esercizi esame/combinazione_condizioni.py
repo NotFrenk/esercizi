cbb657e4ad38353232618fa0e5b3a656ed2522a7
NotFrenk <franciscoleonfelici@gmail.com>
2024-07-25 09:19:20 +0200

    ese3rcizi ripasso esame

--- eserciziRipasso/esercizi esame/combinazione_condizioni.py
+++ eserciziRipasso/esercizi esame/combinazione_condizioni.py
@@ -5,7 +5,10 @@
 #  a seconda delle condizioni che sono soddisfatte.
 
 def check_combination(conditionA: bool, conditionB: bool, conditionC: bool) -> str:
-
+    if conditionA or conditionB and conditionC:
+        return'Operazione permessa'
+    else:
+        return 'Operazione negata'
 
 
 
