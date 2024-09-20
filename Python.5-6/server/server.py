e435969c05d23cc1bb7aac4000764f864119e414
NotFrenk <franciscoleonfelici@gmail.com>
2024-09-20 13:14:24 +0200

    implemento server

--- Python.5-6/server/server.py
+++ Python.5-6/server/server.py
@@ -1,46 +1,94 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for
+from flask import request, render_template
 
-api = Flask(__name__)
 
-utenti: list[list[str]] = [['Mario','Aa@12345','1','Roma','0'],
-                           ['Luigi','Bb$23456','1','Pescara','0'],
-                           ['Francesca','Cc!34567','2','Milano','0']]
+api=Flask(__name__)
+
+utenti = [["mario","rossi","mario.1424@gmail.com","1234","0"],["alessia","garibaldi","alessia.124@yohoo.it","Password01","0"],["gianni","Gianfranco","gianno.898@gmail.com","2304","0"]]
+
+
+
 
 @api.route('/', methods=['GET'])
 def index():
+    return render_template('index2.html')
+
+
+@api.route('/pippo', methods=['GET'])
+def index2():
+    return render_template('registrati.html')
+
+@api.route('/accedi', methods=['GET'])
+def accesso():
+
+    nome = request.args.get("nome")
+    password = request.args.get("password")
+    cognome=request.args.get("cognome")
+
+    if nome and password and cognome:  
+        
+        for utente in utenti:
+            if utente[0] == nome and  utente[3]==password and utente[1]==cognome and utente[-1] == "1"  :
+                
+                return redirect(url_for('accesso_successo', nome=nome, password=password, cognome=cognome))
+
+        return redirect(url_for('accesso_fallito'))
+    return render_template('accedi.html')
+
+
+ 
+
+@api.route('/accesso_successo', methods=['GET'])
+def accesso_successo():
+    nome = request.args.get('nome')
+    cognome = request.args.get('cognome')
+    return render_template('accessoS.html', nome=nome, cognome=cognome)
+
+@api.route('/accesso_fallito', methods=['GET'])
+def accesso_fallito():
+    return render_template('accessoF.html')
+
+
+
+
+
+
+@api.route('/reggistrati', methods=['GET'])
+def registra():
+    nome=request.args.get("nome")
+
+    cognome=request.args.get("cognome")
+
+    email=request.args.get("email")
+
+
+    password=request.args.get("password")
+    l: list[str] =[nome,cognome,email,password,"0"]
+
+    for i in utenti:
+        if l==i:
+            i[-1]="1"
+            return render_template('reg_ok.html')
+        
+    return render_template('reg_ko.html')
+ 
+       
+
+
+
+
+        
+
     
-    return render_template('index.html')
 
-
-@api.route('/regok', methods=['GET'])
+@api.route('/reok', methods=['GET'])
 def reg_ok():
-    
     return render_template('reg_ok.html')
 
-@api.route('/regko', methods=['GET'])
-def reg_ko():
-    
-    return render_template('reg_ko.html')
-
-@api.route('/registrati', methods=['GET'])
-def registra():
-    utente: list[str] = [str(request.args.get("fname")),
-                         str(request.args.get("fpassword")), 
-                         str(request.args.get("fsesso")), 
-                         str(request.args.get("fComune")),
-                         b'0']
-    #print(utente)
-
-    #Devo prendere tutti i dati e verificare se la terna c'è nel vettore degli utenti
-
-    if(utente in utenti):
-        ind: int = utenti.index(utente)
-
-        utenti[ind][4] = '1'
-        return render_template('reg_ok.html')
-    else:
-        return render_template('reg_ko.html')
-    
 
 
-api.run(host='0.0.0.0',port=8085)
+
+
+
+
+api.run(host="0.0.0.0",port=8085)
