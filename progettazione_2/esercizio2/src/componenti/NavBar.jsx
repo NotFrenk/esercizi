13ca54a36c6263204e2238c2ce6387e9ff76c014
NotFrenk <franciscoleonfelici@gmail.com>
2024-11-21 13:03:25 +0100

    nuovo es di progetazione

---
+++ progettazione_2/esercizio2/src/componenti/NavBar.jsx
@@ -0,0 +1,15 @@
+import './NavBar.css'
+function NavBar() {
+   return(
+      <ul>
+         <li><a href="#"> Ciao </a></li>
+         <li><a href="#"> Ciao </a></li>
+         <li><a href="#"> Ciao </a></li>
+         <li><a href="#"> Ciao </a></li>
+         <li><a href="#"> Ciao </a></li>
+         <li><a href="#"> Ciao </a></li>
+         <li><a href="#"> Ciao </a></li>
+      </ul>
+   )
+}
+export default NavBar
