d796b7364f8c99e4a641743a81117a4af78000e2
NotFrenk <franciscoleonfelici@gmail.com>
2024-11-28 19:11:40 +0100

    css e react

--- progettazione_2/esercizio2/src/componenti/NavBar.jsx
+++ progettazione_2/esercizio2/src/componenti/NavBar.jsx
@@ -1,7 +1,25 @@
 import './NavBar.css'
 import Link from './Link'
+const x = 11;
+const img = "vite.svg"
+const img2 = "vite"
+
+const imgStyle = {
+   height: x < 10? "800px" : "100px",
+   borderRadius: "30px"
+}
+
+
 function NavBar() {
    return(
+      <>
+      <div id="boxe" className='rotola'> CIAOOOO</div>
+      <nav>{x.toFixed}</nav>
+      <nav>{x > 1000? "sono in alto!!" : "sono in basso!!"}</nav>
+      <img style={imgStyle} src={img}></img>
+      <img className='bordoarrotondato' src={img2 + ".svg"}></img>
+      <img className='bordoarrotondato' src={`/${img2}.svg`}></img>
+
       <ul>
          <li><Link></Link></li>
          <li><Link></Link></li>
@@ -9,6 +27,7 @@ function NavBar() {
          <li><Link></Link></li>
          <li><Link></Link></li>
       </ul>
+      </>
    )
 }
 export default NavBar
