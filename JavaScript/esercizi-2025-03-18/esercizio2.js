b42b2754061a722a3488b195334612e6ab62df19
NotFrenk <franciscoleonfelici@gmail.com>
2025-03-19 13:18:55 +0100

    minor

--- JavaScript/esercizi-2025-03-18/esercizio2.js
+++ JavaScript/esercizi-2025-03-18/esercizio2.js
@@ -1,7 +1,26 @@
+
 let job = (data) =>{
    return new Promise ( (res, reg) =>{
       if (isNaN(data)){
-         
+            reg("ERRORE");
+      }
+      else if (data % 2 != 0){
+         setTimeout(() => {
+            res("numero disparo")
+         }, 1000);
+      }
+      else{
+         setTimeout(() =>{
+            res("numero paro");
+         }, 2000);
       }
    });
 };
+
+job(1)
+.then((x) =>{
+   console.log(x);
+})
+.catch((e) => {
+   console.log(e);
+})
